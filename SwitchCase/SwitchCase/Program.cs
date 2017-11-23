8de7e6bfca74b6f151c1ac41ee25ee5caddba9d0
Neous1 <Neous1@users.noreply.github.com>
2017-11-23 18:27:59 -0500

    initial commit

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you enjoy C# ? (yes/no/maybe)");
+            string input = Console.ReadLine();
+            switch (input.ToLower())
+            {
+                case "yes":
+                case "maybe":
+                    Console.WriteLine("Great!");
+                    break;
+                case "no":
+                    Console.WriteLine("Too bad!");
+                    break;
+                default:
+                    Console.WriteLine("I'm sorry, I don't understand that!");
+                    break;
+            }
+        }
+    }
+}
