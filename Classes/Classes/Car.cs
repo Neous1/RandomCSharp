efc83dc08f37b06b7c0a2acd90f1d8abd461b36b
Neous1 <Neous1@users.noreply.github.com>
2017-11-28 00:28:55 -0500

    create a method describe() to describe car object

--- Classes/Classes/Car.cs
+++ Classes/Classes/Car.cs
@@ -9,6 +9,12 @@
             this.Color = color;
         }
 
+        //declare a describe() to specify a color.
+        public string Describe()
+        {
+            return "This car is " + Color;
+        }
+
         //declare a property Color to access variable car
         public string Color { get; set; }
     }
