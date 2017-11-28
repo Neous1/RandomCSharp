32c6fe25ae48d58d202a20f40a6b2d04056bc5ea
Neous1 <Neous1@users.noreply.github.com>
2017-11-28 00:01:50 -0500

    create a class create a constructor for the class

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Car
+    {
+        private string color;
+
+        public Car (string color)
+        {
+            this.color = color;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
