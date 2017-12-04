23474c97b7396dc6087c1a57f085f562c5581324
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 22:11:22 -0500

    create a class Rectangle with a constructor that takes 2 params create a GetArea() method

---
+++ RectInheritance/RectInheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectInheritance
+{
+    class Rectangle
+    {
+        //member variables
+        protected double length;
+
+        protected double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
