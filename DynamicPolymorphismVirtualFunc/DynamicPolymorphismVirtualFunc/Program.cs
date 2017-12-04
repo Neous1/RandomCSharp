6983190950be4fe61ba9d3cfc974749ca8abc1c6
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 19:55:53 -0500

    create Class Shape

---
+++ DynamicPolymorphismVirtualFunc/DynamicPolymorphismVirtualFunc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPolymorphismVirtualFunc
+{
+    class Shape
+    {
+        protected int width, height;
+
+        public Shape()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
