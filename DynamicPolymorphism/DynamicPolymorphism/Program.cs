cfe5a6dcecd404623e158a00cfbeaa40f2dae45a
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 19:30:06 -0500

    create a class shape and class rectange derived from shape

---
+++ DynamicPolymorphism/DynamicPolymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPolymorphism
+{
+    class Program
+    {
+        abstract class Shape
+        {
+            public abstract int area();
+        }
+
+       public class Rectangle: Shape
+       {
+            private int length;
+           private int width;
+
+           public Rectangle(int a = 0, int b = 0)
+           {
+                length = a;
+               width = b;
+           }
+
+           public override int area()
+           {
+                Console.WriteLine("Rectangle class are:");
+               return (width * length);
+           }
+
+       }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
