90377980a32ac0ccf55db3f0b07fc1931f1ebad2
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 22:23:47 -0500

    create class Tabletop derived from Rectangle create constructor for tabletop with l and w as params derived from rectangle's constructor

--- RectInheritance/RectInheritance/Program.cs
+++ RectInheritance/RectInheritance/Program.cs
@@ -1,36 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RectInheritance
 {
-    class Rectangle
+    class Tabletop: Rectangle
     {
-        //member variables
-        protected double Length;
+        private double cost;
 
-        protected double Width;
-
-        public Rectangle(double l, double w)
+        public Tabletop(double l, double w): base(l, w)
         {
-            Length = l;
-            Width = w;
+            
         }
-
-        public double GetArea()
-        {
-            return Length * Width;
-        }
-        public void Display()
-        {
-            Console.WriteLine("Length: {0}", Length);
-            Console.WriteLine("Width: {0}", Width);
-            Console.WriteLine("Area: {0}", GetArea());
-
-        }
-
     }
     class Program
     {
---
+++ RectInheritance/RectInheritance/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RectInheritance
+{
+    public class Rectangle
+    {
+        //member variables
+        protected double Length;
+
+        protected double Width;
+
+        public Rectangle(double l, double w)
+        {
+            Length = l;
+            Width = w;
+        }
+
+        public double GetArea()
+        {
+            return Length * Width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", Length);
+            Console.WriteLine("Width: {0}", Width);
+            Console.WriteLine("Area: {0}", GetArea());
+
+        }
+
+    }
+}
