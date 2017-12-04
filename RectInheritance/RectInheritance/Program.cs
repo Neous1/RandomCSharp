6bd926cffd5e4e7c36802e130707117c7ff8d7d4
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 22:19:12 -0500

    create a Display () for class Rectangle

--- RectInheritance/RectInheritance/Program.cs
+++ RectInheritance/RectInheritance/Program.cs
@@ -9,21 +9,27 @@ namespace RectInheritance
     class Rectangle
     {
         //member variables
-        protected double length;
+        protected double Length;
 
-        protected double width;
+        protected double Width;
 
         public Rectangle(double l, double w)
         {
-            length = l;
-            width = w;
+            Length = l;
+            Width = w;
         }
 
         public double GetArea()
         {
-            return length * width;
+            return Length * Width;
         }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", Length);
+            Console.WriteLine("Width: {0}", Width);
+            Console.WriteLine("Area: {0}", GetArea());
 
+        }
 
     }
     class Program
