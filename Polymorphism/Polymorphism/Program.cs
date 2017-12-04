ea48650a8583e3b447f41559d3edafa899187e8f
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 19:08:06 -0500

    create a class printdata create a print method

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Printdata
+    {
+        void print(int i)
+        {
+            Console.WriteLine("printing int : {0}", i);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
