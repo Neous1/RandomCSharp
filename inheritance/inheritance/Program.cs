875491d34a474de44b9d6526ba356498fc66a574
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 19:04:39 -0500

    inheritance and derived classes

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    //Derived class
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle();
+
+            rectangle.setWidth(5);
+            rectangle.setHeight(7);
+
+            //Print the area of the object
+            Console.WriteLine("Total area: {0}", rectangle.GetArea());
+
+        }
+    }
+}
---
+++ inheritance/inheritance/Shape.cs
@@ -0,0 +1,18 @@
+﻿namespace inheritance
+{
+    public class Shape
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+
+        protected int width;
+        protected int height;
+    }
+}
