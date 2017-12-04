e700e9711e32c27dab4a7d4009e4d187975f66c3
Neous1 <Neous1@users.noreply.github.com>
2017-12-03 19:18:08 -0500

    call print() from main to perform 3 distinct functions

--- Polymorphism/Polymorphism/Program.cs
+++ Polymorphism/Polymorphism/Program.cs
@@ -6,21 +6,42 @@ using System.Threading.Tasks;
 
 namespace Polymorphism
 {
-    class Printdata
+    public class Printdata
     {
-        void print(int i)
+        public void Print(int i)
         {
             Console.WriteLine("printing int : {0}", i);
         }
-        void print(double f)
+
+        public void Print(double f)
         {
             Console.WriteLine("Printing float: {0}", f);
         }
+
+        public void Print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+
+
+
     }
     class Program
     {
         static void Main(string[] args)
         {
+            var p = new Printdata();
+            
+            //call print to print integer
+            p.Print(5);
+
+            //call print to print float 
+            p.Print(500.27);
+
+            //call print to print string
+            p.Print("Hellow SiliconRhode");
+
+
         }
     }
 }
