35dbe580b186dbc3233180c03baf6bf3ba2d55e6
Neous1 <Neous1@users.noreply.github.com>
2017-12-08 22:01:28 -0500

    create printdata class function/method overloading of print

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Printdata
+    {
+        public void Print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", 
+                i);
+        }
+
+        public void Print(double f)
+        {
+            Console.WriteLine("Printing int: {0}",
+                f);
+        }
+
+        public void Print(string s)
+        {
+            Console.WriteLine("Printing int: {0}",
+                s);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Printdata();
+            p.Print(5);
+
+            //print float
+            p.Print(500.263);
+
+            //print string
+            p.Print("Hello c++");
+
+            Console.ReadKey();
+
+        }
+    }
+}
