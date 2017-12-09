d0d162f4c78299a14e584693acddaf4ae44e83ac
Neous1 <Neous1@users.noreply.github.com>
2017-12-08 22:42:32 -0500

    create an abstract class with abstract method to be fully implement in a derived subclass

---
+++ Polymorphism/PolymorphismApplication/PolymorphismApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PolymorphismApplication
+{
+    //partially implemented class Shape is created with 
+    //used of the keyworld abstract.
+
+    abstract class Shape
+    {
+        public abstract int Area();
+    }
+
+    class Rectangle: Shape
+    {
+        public int Length { get; }
+        public int Width { get; }
+
+        public Rectangle(int a = 0, int b = 0)
+        {
+            Length = a;
+            Width = b;
+        }
+
+        public override int Area()
+        {
+            Console.WriteLine("Rectangle class area:");
+            return (Width * Length);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var rect = new Rectangle(10, 7);
+            double a = rect.Area();
+            Console.WriteLine("Area: {0}", a);
+            Console.ReadKey();
+        }
+    }
+}
