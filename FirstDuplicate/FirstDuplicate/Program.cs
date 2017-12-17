286779d5d9bf3234180f67b0b22f250a0b48afa7
Neous1 <Neous1@users.noreply.github.com>
2017-12-16 22:18:35 -0500

    nested loop

---
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = {2, 3, 1, 5, 2};
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("a[i] is " + a[i]);
+                for (var j = 1; j < a.Length;)
+                {
+                  //  Console.WriteLine("value is : " + a[j]);
+                    if (a[i] == a[j])
+                    {
+                        Console.WriteLine("a[i] is {0} a[j] is {1} index is {2}", a[i], a[j], i);
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
