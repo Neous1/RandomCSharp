bbebf36cd75d5b3f011a26d32ccbfd043f871a2f
Neous1 <Neous1@users.noreply.github.com>
2017-12-18 13:12:43 -0500

    moved student class

--- Properties/Properties/Program.cs
+++ Properties/Properties/Program.cs
@@ -6,25 +6,14 @@ using System.Threading.Tasks;
 
 namespace Properties
 {
-    class Student
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public override string ToString()
-        {
-            return "Code = "+Code +", Name = "+ Name+ ", Age = " + Age;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             var student = new Student();
             student.Code = "0001";
-            student.Name = "Zara";
-            student.Age = 9;
+            student.Name = "";
+            //student.Age = 9;
             Console.WriteLine("Student Info: {0}", student);
         }
     }
---
+++ Properties/Properties/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace Properties
+{
+    class Student
+    {
+        private string _code = "N/A";
+        private string _name;
+        private int _age;
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = "unknown";
+                }
+                _name = value; 
+                
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Code = "+Code +", Name = "+ Name+ ", Age = " + Age;
+        }
+    }
+}
