4b2ed443524ad03bc899e6ccc1d4f25e2512ffaa
Neous1 <Neous1@users.noreply.github.com>
2017-12-19 08:59:18 -0500

    clean

--- FirstDuplicate/FirstDuplicate/Program.cs
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -10,14 +10,22 @@ namespace FirstDuplicate
     {
         static void Main(string[] args)
         {
-            int[] a = {2, 3, 1, 2, 5,3}; 
+            int[] a = {2, 3, 1, 2, 5,3};
+            int curVal;
+            int curPos
 
             for (int i = 0; i < a.Length; i++)
+            {
+               
+            }
+
+/*            for (int i = 0; i < a.Length; i++)
             {
                 for (int j = 1; j < a.Length; j++)
                 {
                     if (a[i] == a[j] && (i != j))
                     {
+                        
                         //Console.WriteLine("the values for i is {0} and js is {1}", a[i], a[j]);
                         //Console.WriteLine("the index for i is {0} and js is {1}", i , j);
                         Console.WriteLine(a[j]);
@@ -27,7 +35,7 @@ namespace FirstDuplicate
 
                     
                 }
-            }
+            }*/
 
 
             Console.ReadLine();
