61f3fceaf5ee29a7b2fd5a98eac511a6d6645a41
Neous1 <Neous1@users.noreply.github.com>
2017-12-19 23:59:51 -0500

    more cleaning

--- FirstDuplicate/FirstDuplicate/Program.cs
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -11,31 +11,27 @@ namespace FirstDuplicate
         static void Main(string[] args)
         {
             int[] a = {2, 3, 1, 2, 5,3};
-            int curVal;
-            int curPos
+           //
+            
+               var dupList = new List<int>();
 
-            for (int i = 0; i < a.Length; i++)
+            foreach (int t in a)
             {
-               
+                int curVal = t;
+                int curPos;
+                //if ()
+                
+                dupList.Add(t);
+                Console.WriteLine(t);
             }
+            
 
-/*            for (int i = 0; i < a.Length; i++)
-            {
-                for (int j = 1; j < a.Length; j++)
-                {
-                    if (a[i] == a[j] && (i != j))
-                    {
-                        
-                        //Console.WriteLine("the values for i is {0} and js is {1}", a[i], a[j]);
-                        //Console.WriteLine("the index for i is {0} and js is {1}", i , j);
-                        Console.WriteLine(a[j]);
-                        
-                    }
+
                     
 
                     
                 }
-            }*/
+            
 
 
             Console.ReadLine();
