e6e9a0f88749d3ee13402cea69b87273a1c0ae50
Neous1 <Neous1@users.noreply.github.com>
2017-12-23 00:01:33 -0500

    Overload + operator to add two Box objects

--- OperatorOverloading/OperatorOverloading/Program.cs
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -33,6 +33,14 @@ namespace OperatorOverloading
         }
 
         //OVerload + operator to add two Box objects
+        public static Box operator+ (Box b, Box c)
+        {
+            Box box = new Box();
+            box.length = b.length + c.length;
+            box.breadth = b.breadth + c.breadth;
+            box.height = b.height + c.height;
+            return box;
+        }
         
 
         
