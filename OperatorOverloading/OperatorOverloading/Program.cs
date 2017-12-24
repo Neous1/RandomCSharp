21f728ff7c42a3d94f1518cc92008022b3ceaf5c
Neous1 <Neous1@users.noreply.github.com>
2017-12-23 23:53:48 -0500

    create class box add getVolume method

---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Box
+    {
+        private double length;
+        private double breadth;
+        private double height;
+
+        public double getVolume()
+        {
+            return length * breadth * height;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
