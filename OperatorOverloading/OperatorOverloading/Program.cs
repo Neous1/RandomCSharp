906e6a07ec6c20db04b2a9e67b4e27722e15d410
Neous1 <Neous1@users.noreply.github.com>
2017-12-25 00:13:08 -0500

    Operator overloadding " >"

--- OperatorOverloading/OperatorOverloading/Program.cs
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -62,6 +62,12 @@ namespace OperatorOverloading
             return status;
         }
 
+        public static bool operator > (Box lhs, Box rhs)
+        {
+            bool status = lhs.length > rhs.length || lhs.height > rhs.height || lhs.breadth > rhs.breadth;
+            return status;
+        }
+
 
 
 
