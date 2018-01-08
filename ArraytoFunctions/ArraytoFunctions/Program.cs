544e5340e008cdb8f7cf37471dd2e6836b1eaceb
Neous1 <Neous1@users.noreply.github.com>
2018-01-07 23:11:34 -0500

    creat class myArray create method getAverage() that takes an array of type int

---
+++ ArraytoFunctions/ArraytoFunctions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraytoFunctions
+{
+    class MyArray {
+        double getAverage(int[] arr, int size)
+        {
+            double avg;
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
