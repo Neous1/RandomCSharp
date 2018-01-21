57d713c8d8a50900fafca82480e9bc6883f8fdc5
Neous1 <Neous1@users.noreply.github.com>
2018-01-21 14:13:13 -0500

    create customer class

---
+++ Dictionary_Demo/Dictionary_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Demo
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
