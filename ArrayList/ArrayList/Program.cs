914347fe1d51866c2246b85947ebf58ff6291c65
Neous1 <Neous1@users.noreply.github.com>
2018-01-25 12:28:54 -0500

    create an ArrayList add some numbers to the arraylist

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList al = new System.Collections.ArrayList();
+
+            Console.WriteLine("Adding some numbers");
+            al.Add(45);
+            al.Add(78);
+            al.Add(33);
+            al.Add(56);
+            al.Add(12);
+            al.Add(23);
+            al.Add(9);
+        }
+    }
+}
