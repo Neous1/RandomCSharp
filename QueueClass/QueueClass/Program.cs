ba9b74f94af791e3ae20bed6d941e603522a8289
Neous1 <Neous1@users.noreply.github.com>
2018-01-27 21:45:32 -0500

    clean up  - remove stack class codes

--- QueueClass/QueueClass/Program.cs
+++ QueueClass/QueueClass/Program.cs
@@ -11,31 +11,7 @@ namespace QueueClass
     {
         static void Main(string[] args)
         {
-           /* Queue q = new Queue();
-
-            q.Enqueue('A');
-            q.Enqueue('M');
-            q.Enqueue('G');
-            q.Enqueue('W');
-
-            Console.WriteLine("Current queue:");
-            foreach (char c in q)
-            {
-            }
-            
-            Console.WriteLine();
-            q.Enqueue('V');
-            q.Enqueue('H');
-            Console.WriteLine("Current queue: ");
-            foreach (char c in q) Console.Write(c + " ");
-            Console.WriteLine();
-            Console.WriteLine("Removing some values ");
-            char ch = (char)q.Dequeue();
-            Console.WriteLine("The removed value: {0}", ch);
-            ch = (char)q.Dequeue();
-            Console.WriteLine("The removed value: {0}", ch);
-
-            Console.ReadKey();*/
+          
 
             Queue<string> numbers = new Queue<string>();
             //add these strings to the queue.
