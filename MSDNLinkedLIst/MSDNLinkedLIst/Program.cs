44ff2ad4a57f4e949409b902af1d802274dd5763
Neous1 <Neous1@users.noreply.github.com>
2018-01-29 11:47:01 -0500

    create a linkedLIst

---
+++ MSDNLinkedLIst/MSDNLinkedLIst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDNLinkedLIst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the link list
+            string[] words = {"the", "fox", "jumped", "over", "the", "dog"};
+            LinkedList<String> sentence = new LinkedList<string>(words);
+           
+        }
+    }
+}
