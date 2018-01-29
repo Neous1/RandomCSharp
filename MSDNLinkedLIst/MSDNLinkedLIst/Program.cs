b46f95c3b2a9e7509f0f0e8c439ca5c18e7bb6bf
Neous1 <Neous1@users.noreply.github.com>
2018-01-29 12:17:02 -0500

    add the word today in the beginning of the linked list

--- MSDNLinkedLIst/MSDNLinkedLIst/Program.cs
+++ MSDNLinkedLIst/MSDNLinkedLIst/Program.cs
@@ -16,6 +16,11 @@ namespace MSDNLinkedLIst
             Display(sentence, "The linked list values:");
             Console.WriteLine("sentence.Contains('jumped') = {0}", sentence.Contains("jumped"));
 
+            //Add the world "today" to the beginning of hte linked list.
+            sentence.AddFirst("today");
+            Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+
+            
         }
 
         private static void Display(LinkedList<string> words, string test)
