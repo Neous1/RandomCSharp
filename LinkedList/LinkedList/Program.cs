74c053f884bc19c7164188f6021ae902bd8ab31f
Neous1 <Neous1@users.noreply.github.com>
2018-01-28 23:05:29 -0500

    create a node class

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+ 
+    //create node class
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public void displayNOde()
+        {
+            Console.WriteLine("<" + data + ">");
+        }
+    }
+}
