f5e8ba21873af73f653d9626edcb21d78624cc66
Neous1 <Neous1@users.noreply.github.com>
2018-01-29 12:17:31 -0500

    Move the first node to be the last node.

--- MSDNLinkedLIst/MSDNLinkedLIst/Program.cs
+++ MSDNLinkedLIst/MSDNLinkedLIst/Program.cs
@@ -16,11 +16,17 @@ namespace MSDNLinkedLIst
             Display(sentence, "The linked list values:");
             Console.WriteLine("sentence.Contains('jumped') = {0}", sentence.Contains("jumped"));
 
-            //Add the world "today" to the beginning of hte linked list.
+            //Add the word "today" to the beginning of hte linked list.
             sentence.AddFirst("today");
             Display(sentence, "Test 1: Add 'today' to beginning of the list:");
 
-            
+            //Move the first node to be the last node.
+
+            LinkedListNode<string> mark1 = sentence.First;
+            sentence.RemoveFirst();
+            sentence.AddLast(mark1);
+            Display(sentence, "Test 2: Move first node to be last node:");
+
         }
 
         private static void Display(LinkedList<string> words, string test)
