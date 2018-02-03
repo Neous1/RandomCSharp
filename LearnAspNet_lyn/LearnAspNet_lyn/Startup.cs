641c71acb992af6198a78c33046e9370807483d0
Neous1 <Neous1@users.noreply.github.com>
2018-02-03 09:51:28 -0500

    initial commit, Add HelloWorld page

---
+++ LearnAspNet_lyn/LearnAspNet_lyn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnAspNet_lyn.Startup))]
+namespace LearnAspNet_lyn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,78 +10,17 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            SinglyLinkedList myList = new SinglyLinkedList();
-            myList.InsertFirst(100);
-            myList.InsertFirst(50);
-            myList.InsertFirst(99);
-            myList.InsertFirst(88);
-            myList.insertLast(999000);
-            myList.displayList();
-
-            myList.DeleteFirst();
-            myList.displayList();
-
         }
     }
 
-    public class SinglyLinkedList
-    {
-        private Node _first;
-
-        public bool IsEmpty()
-        {
-            return (_first == null);
-        }
-
-        public void InsertFirst(int data)
-        {
-            Node newNode = new Node();
-            newNode.data = data;
-            newNode.next = _first;
-            _first = newNode;
-        }
-
-        public Node DeleteFirst()
-        {
-            Node temp = _first;
-            _first = _first.next;
-            return temp;
-        }
-
-        public void displayList()
-        {
-            Console.WriteLine("LIst(first -- last)");
-            Node current = _first;
-            while (current != null)
-            {
-                current.displayNode();
-                current = current.next;
-            }
-            Console.WriteLine();
-        }
-
-        public void insertLast(int data
-            )
-        {
-            Node current = _first;
-            while (current.next != null)
-            {
-                current = current.next;
-            }
-
-            Node newNode = new Node();
-            newNode.data = data;
-            current.next = newNode;
-        }
-
-    }
+ 
     //create node class
     public class Node
     {
         public int data;
         public Node next;
 
-        public void displayNode()
+        public void displayNOde()
         {
             Console.WriteLine("<" + data + ">");
         }
