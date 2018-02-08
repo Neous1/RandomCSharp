f7770965b1980609ad7c1b62902010348aeca358
Neous1 <Neous1@users.noreply.github.com>
2018-02-08 09:49:46 -0500

    create a struct Rectangle with a constructor and a method

---
+++ StructClasses/StructClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            //create a constructor
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+            
+        }
+    }
+}
