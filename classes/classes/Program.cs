46dc17cf3e01b5014f702ff5beeb43bd67532fbf
Neous1 <Neous1@users.noreply.github.com>
2018-02-09 15:11:43 -0500

    create a method to makeSound create a method to get numofanimals

--- classes/classes/Program.cs
+++ classes/classes/Program.cs
@@ -15,14 +15,27 @@ namespace Classes
         {
             name = "No Name";
             sound = "No Sound";
+            numOfAnimals++;
         }
 
         public Animal(string name = "No Name")
         {
             this.name = name;
             this.sound = "No Sound";
+            numOfAnimals++;
         }
 
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says{1}", name, sound);
+        }
+
+         static int numOfAnimals = 0;
+
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
     }
     class Program
     {
